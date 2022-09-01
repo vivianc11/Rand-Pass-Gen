0f306e87481e04d33a681ed3ab055e2bf6d654ca
Vivian Chen <vivian.chen1193@gmail.com>
2022-08-31 21:31:06 -0700

    fixed problem with form not clearing after pushing button

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-var password = "";
+
 
 //generating a random character using ASCII
 function getCharacter() {
@@ -7,32 +7,29 @@ function getCharacter() {
   return char;
 }
 
-
 function generatePassword() {
+ 
   var userNum = window.prompt('How many characters do you want your password to have? Choose a number between 8 - 128');
-  
-  for (let i = 0; i <= userNum; i++) { 
+  var password = ""
+  for (let i = 1; i <= userNum; i++) { 
     var char = getCharacter();
     password += char;
-  } return password;
-
   }
-
-
-
+  return password;
+}
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  document.getElementById("password").value = "";
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", clea)
 generateBtn.addEventListener("click", writePassword);

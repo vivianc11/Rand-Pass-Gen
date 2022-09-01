6f0463f5e762e09cc99c9a0fb5d0e137f7089968
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-01 09:59:23 -0700

    changed how password is formed, put characters into a string and adding those to an overall string based on the user's selections of criteria

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,50 @@
-
-
-//generating a random character using ASCII
-function getCharacter() {
-  var asciiNum = Math.floor((Math.random()* 94)+ 33);
-  var char = String.fromCharCode(asciiNum);
-  return char;
-}
-
 function generatePassword() {
+  var symbols = "~!@#$%^*-_=+[{]}/;:,.?";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var none = "";
   var capChoice = window.prompt ('Would you like your password to have capital letters? (Yes or No)')
-  // var numChoice = window.prompt ('Would you like your password to have numbers? (Yes or No)')
-  // var symbolChoice = window.prompt ('Would you like to your password to have symbols? (Yes or No)')
+  var numChoice = window.prompt ('Would you like your password to have numbers? (Yes or No)')
+  var symbolChoice = window.prompt ('Would you like to your password to have symbols? (Yes or No)')
   var userNum = window.prompt('How many characters do you want your password to have? Choose a number between 8 - 128');
+  var options = ""
   var password = ""
-  for (let i = 1; i <= userNum; i++) { 
-    var char = getCharacter();
-    password += char;
-  }
-  if (capChoice === 'No'){
-    var noCapPass = password.toLowerCase();
-    return noCapPass;
+  capChoice = capChoice.toLowerCase();
+  numChoice = numChoice.toLowerCase();
+  symbolChoice = symbolChoice.toLowerCase();
+  options += lowerCase;
+
+  if (capChoice === "yes") {
+    options += upperCase;
   } else {
-    return password;
+    options += none;
   }
   
+  if (numChoice === "yes") {
+    options += numbers;
+  } else {
+    options += none;
+  }
+
+  if (symbolChoice === "yes") {
+    options += symbols;
+  } else {
+    options += none;
+  }
+
+  function getCharacter() {
+    var index = Math.floor((Math.random()* options.length));
+    var char = options.charAt(index);
+    return char;
+  }
+
+  for (let i = 0; i <= userNum; i++) {
+    var char = getCharacter();
+    password += char;
+    
+  }
+  return password;
 }
   
 

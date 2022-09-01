71a07c2587876a3dbf6aed945ead74bbd12e77fd
Vivian Chen <vivian.chen1193@gmail.com>
2022-08-31 19:29:29 -0700

    wrote function for randomly creating a character

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,24 @@
 // Assignment code here
+//generating a random character using ASCII
+function getCharacter() {
+  var asciiNum = Math.floor((Math.random()* 94)+ 33);
+  var char = String.fromCharCode(asciiNum);
+  return char;
+}
+
+function generatePassword() {
+  var password = "";
+  var char = getCharacter();
+  password += char;
+
+  for (let i = 1; i <= 12; i++) {
+    
+  }
+  }
+
+
+
+}
 
 
 // Get references to the #generate element
